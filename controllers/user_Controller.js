c56f8969826a3ff13539b159512340f9b52abfba
TFA21SCM65M <tmalhotra@hawk.iit.edu>
2022-01-19 02:50:38 -0600

    Sign-up code added

--- controllers/user_Controller.js
+++ controllers/user_Controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -14,4 +16,30 @@ module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
         title: "Meta | Sign-in"
     });
+}
+
+
+//getting the sign-up data
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    } 
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating while creation'); return}
+
+                return res.redirect('/users/Sign-in');
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+}
+
+// Sign in and create a session for the user
+module.exports.createSession = function(req, res){
+
 }
--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,7 @@ router.get('/profile', usersController.profile);
 router.get('/Sign-up', usersController.signUp);
 router.get('/Sign-in', usersController.signIn);
 
+router.post('/create', usersController.create);
+
 
 module.exports = router;
